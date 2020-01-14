b7b2d398eecdb527655726f488f758c7aab2c8c9
dionybusker <99057959@mydavinci.nl>
2020-01-14 10:11:08 +0100

    kleuren en pins gedeelte styling gedaan

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ var checkPin = document.createElement("div");
     guessContainer.appendChild(checkPin);
 
 for (var rows = 12; rows > 0; rows--) {
+    // kleuren gedeelte
     var divGuesses = document.createElement("div");
         divGuesses.setAttribute("id", `row_${rows}`);
         guesses.appendChild(divGuesses)
@@ -21,19 +22,18 @@ for (var rows = 12; rows > 0; rows--) {
             colorBox.style.margin = "5px";
             colorBox.style.borderRadius = "15px";
             divGuesses.appendChild(colorBox);
-
-            
     }
 
+    // pins gedeelte
     var divPins = document.createElement("div");
         divPins.setAttribute("id", `row_${rows}`);
         checkPin.appendChild(divPins);
     for (var column = 0; column < 4; column++) {
         var pin = document.createElement("div");
-            pin.style.border = "1px solid red";
-            pin.style.width = "15px";
-            pin.style.height = "10px";
-            pin.style.margin = "0 5px";
+            pin.style.border = "1px solid black";
+            pin.style.width = "7px";
+            pin.style.height = "15px";
+            pin.style.margin = "10px 5px";
             pin.style.display = "inline-block";
             divPins.appendChild(pin);
     }
