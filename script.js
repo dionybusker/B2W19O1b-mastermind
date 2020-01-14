7196e7494577c36d49abcade5ac2cc37674a412c
dionybusker <99057959@mydavinci.nl>
2020-01-14 13:19:22 +0100

    border(radius), margin aangepast

--- script.js
+++ script.js
@@ -13,14 +13,14 @@ for (var rows = 12; rows > 0; rows--) {
     var divGuesses = document.createElement("div");
         divGuesses.setAttribute("id", `row_${rows}`);
         guesses.appendChild(divGuesses)
-    for (var column = 0; column < 6; column++) {
+    for (var column = 0; column < 4; column++) {
         var colorBox = document.createElement("div");
-            colorBox.style.border = "1px solid red";
+            colorBox.style.border = "1px solid black";
             colorBox.style.width = "40px";
             colorBox.style.height = "25px";
             colorBox.style.display = "inline-block";
             colorBox.style.margin = "5px";
-            colorBox.style.borderRadius = "15px";
+            colorBox.style.borderRadius = "0 15px";
             divGuesses.appendChild(colorBox);
     }
 
@@ -33,7 +33,7 @@ for (var rows = 12; rows > 0; rows--) {
             pin.style.border = "1px solid black";
             pin.style.width = "7px";
             pin.style.height = "15px";
-            pin.style.margin = "10px 5px";
+            pin.style.margin = "10px 4px";
             pin.style.display = "inline-block";
             divPins.appendChild(pin);
     }
